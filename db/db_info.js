95ceecf6475b6c62e03f6c4e47c7ea5f18d3520e
SangHwaLeeSoas <sangwha0924@gmail.com>
2020-11-01 17:26:22 +0900

    MYSQL 적용

---
+++ db/db_info.js
@@ -0,0 +1,11 @@
+module.exports = (function () {
+    return {
+        local: { // localhost
+            host: '127.0.0.1',
+            port: '3306',
+            user: 'root',
+            password: 'qhdks12#$',
+            database: ''
+        }
+    }
+})();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+/* DB */
+var mysql_dbc = require('../db/db_con')();
+var connection = mysql_dbc.init();
+mysql_dbc.test_open(connection);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("HOME 진입");
   res.render('index', { title: 'Express' });
+});
+
+/* TEST */
+router.get('/mysql/test', function (req, res) {
+  console.log("TSET 진입");
+  var stmt = 'SELECT NOW()';
+  connection.query(stmt, function (err, result) {
+  console.log(result);
+  })
 });
 
 module.exports = router;
