4551994b3c81fc28a8cd586c96c0589d800a75f4
SangHwaLeeSoas <sangwha0924@gmail.com>
2020-11-03 23:45:50 +0900

    1. 날씨 정보 크롤링 2. 메인 화면 카드 및 아이콘 적용

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,15 @@ $(document).ready(function(){
       type: 'POST',
       success: function(result) {
          console.log(result);
+         $('#temp').text(result.temp + ' ℃');
+         $('#realTemp').text(result.realTemp + ' ℃');
+         $('#airName').text(result.airName);
+         $('#icon').attr('src', 'https://www.accuweather.com' + result.icon);
+
+         if(result.airNum){
+            // TODO : 마스크 착용
+         }
+
       }
    });
 
--- routes/index.js
+++ routes/index.js
@@ -25,20 +25,42 @@ router.post('/init', async (req, res, next) => {
   axios.get(url).then(html => {
     console.log('====================================');
     let ulList = [];
-    const $ = cheerio.load(html.data);
-    const $tempList = $(".temp-container").find(".temp").text().split('°');
+    let $ = cheerio.load(html.data);
+    let $tempList = $(".temp-container").find(".temp").text().split('°');
     console.log($tempList[0].replace(/\D/g, ''));
     console.log($tempList[1].replace(/\D/g, ''));
 
-    const $airCnt = $(".air-quality-content").find(".aq-number").text();
+    let $icon = $("img.weather-icon").attr('data-src');
+    console.log($icon);
+
+    let $iconNum = $icon.split('weathericons/')[1].split('.')[0];
+    console.log('$iconNum: ', $iconNum);
+    let $iconData = 0;
+    if(12 <= Number($iconNum) && Number($iconNum) <= 26){
+      $iconData = $iconNum;
+    }
+
+    console.log('$iconData: ', $iconData);
+
+    let $airCnt = $(".air-quality-content").find(".aq-number").text();
     console.log($airCnt.replace(/\D/g, ''));
 
-    const $airName = $(".air-quality-data-wrapper").find(".category-text").text();
+    let $airName = $(".air-quality-data-wrapper").find(".category-text").text();
     console.log($airName);
 
     //json으로 변환하여 app으로 전송
-    console.log('====================================');
-    return res.json(data);
+    let dateList = {
+      temp : $tempList[0].replace(/\D/g, ''),
+      realTemp : $tempList[1].replace(/\D/g, ''),
+      icon : $icon,
+      iconNum : $iconData,
+      airNum : $airCnt.replace(/\D/g, ''),
+      airName : $airName
+    };
+
+
+
+    return res.json(dateList);
   })
 
 });
